abdede07fd63d1b591d0c07a6e1a11f192534f5a
jontmy00 <jontmy001@gmail.com>
2021-05-02 14:24:14 +0800

    Complete day 3 part 1

--- app/Day3.hs
+++ app/Day3.hs
@@ -32,16 +32,12 @@ intersectsLine :: Line -> Line -> Bool
 intersectsLine line1 line2 = do
   -- case 1: both lines are vertical
   if isVertical line1 && isVertical line2
-    then if x (fst line1) /= x (fst line2)
-      then False
-      else (y $ fst line1, y $ snd line1) `intersectsRange` (y $ fst line2, y $ snd line2)
+    then False
   -- case 2: both lines are horizontal
   else if isHorizontal line1 && isHorizontal line2
-    then if y (fst line1) /= y (fst line2)
-      then False
-      else (x $ fst line1, x $ snd line1) `intersectsRange` (x $ fst line2, x $ snd line2)
+    then False
   -- case 3: line1 is vertical and line2 is horizontal
-  else if (isVertical line1) && (isHorizontal line2)
+  else if isVertical line1 && isHorizontal line2
     then (x $ fst line1) `liesOnRange` (x $ fst line2, x $ snd line2) && (y $ fst line2) `liesOnRange` (y $ fst line1, y $ snd line1)
   -- case 4: line1 is horizontal and line2 is vertical
   else (y $ fst line1) `liesOnRange` (y $ fst line2, y $ snd line2) && (x $ fst line2) `liesOnRange` (x $ fst line1, x $ snd line1)
@@ -70,15 +66,30 @@ parseMove prev str = do
     then (prev, (x prev, (y prev) - distance))
   else if direction == 'L'
     then (prev, ((x prev) - distance, y prev))
-  else (prev, ((x prev) + distance, y prev))
-  
+  else if direction == 'R'
+    then (prev, ((x prev) + distance, y prev))
+  else error "Invalid direction in move string."
+
 -- Takes a line and a list of lines, finding the lines in the list that intersect with the first line.
 intersections :: Line -> [Line] -> [Line]
 intersections path paths = filter (\pathChecking -> path `intersectsLine` pathChecking) paths
 
 -- Takes 2 lines and finds the point at which they intersect.
 pointOfIntersection :: Line -> Line -> Coordinates
-pointOfIntersection first second = (0, 0)
+pointOfIntersection line1 line2 = do
+  if isVertical line1 && isHorizontal line2
+    then (x $ fst line1, y $ fst line2)
+  else if isHorizontal line1 && isVertical line2
+    then (x $ fst line2, y $ fst line1)
+  else error "Assumed that parallel lines do not have a point of intersection."
+
+-- Takes a tuple of a line and a list of lines, finding all the points at which the line intersects with lines in the list.
+pointsOfIntersection :: (Line, [Line]) -> [Coordinates]
+pointsOfIntersection (line1, line2s) = map (\line2 -> pointOfIntersection line1 line2) line2s
+
+-- Takes a point and calculates its Manhattan distance from the origin (0, 0).
+manhattanDistance :: Coordinates -> Int
+manhattanDistance p = (abs $ x p) + (abs $ y p)
 
 -- Solves by line intersections rather than a 2D list of points.
 solvePartOne :: IO Integer
@@ -86,9 +97,9 @@ solvePartOne = do
   wires <- readStrings "app/input/Day3.txt"
   let first = parseMoves $ head wires
   let second = parseMoves $ (wires !! 1)
-  let allIntersections = zip first (map (\path -> intersections path second) first)
-  mapM_ print allIntersections
-  return 0
+  let lineIntersections = zip first (map (\path -> intersections path second) first)
+  let pointIntersections = concat $ filter (\lst -> length lst /= 0) $ map pointsOfIntersection lineIntersections
+  return $ fromIntegral $ foldr1 min $ map (\p -> manhattanDistance p) pointIntersections
 
 solvePartTwo :: IO Integer
 solvePartTwo = return 0
