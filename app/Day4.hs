4d8a665652d5daf714a230c3731cccc5a4d6b297
jontmy00 <jontmy001@gmail.com>
2021-05-03 11:15:56 +0800

    Complete day 4 part 2

--- app/Day4.hs
+++ app/Day4.hs
@@ -1,6 +1,7 @@
 module Day4 where
- 
+
 import Data.Char(digitToInt)
+import Data.Map
 
 twoSameAdjacentDigits :: Int -> Bool
 twoSameAdjacentDigits n = a || b || c || d || e
@@ -11,7 +12,7 @@ twoSameAdjacentDigits n = a || b || c || d || e
     c = str !! 2 == str !! 3
     d = str !! 3 == str !! 4
     e = str !! 4 == str !! 5
-    
+
 noDecreasingDigits :: Int -> Bool
 noDecreasingDigits n = a && b && c && d && e
   where
@@ -21,14 +22,26 @@ noDecreasingDigits n = a && b && c && d && e
     c = digitToInt (str !! 2) <= digitToInt (str !! 3)
     d = digitToInt (str !! 3) <= digitToInt (str !! 4)
     e = digitToInt (str !! 4) <= digitToInt (str !! 5)
-    
+
 meetsBothCriteria :: Int -> Bool
 meetsBothCriteria n = twoSameAdjacentDigits n && noDecreasingDigits n
 
 solvePartOne :: IO Integer
 solvePartOne = return (fromIntegral (length result))
   where
-    result = filter meetsBothCriteria [123257..647015]
-    
+    result = Prelude.filter meetsBothCriteria [123257..647015]
+
+digitFrequencies :: Int -> [(Char, Int)]
+digitFrequencies n = toList $ fromListWith (+) [(c, 1) | c <- show n]
+
+noLargerMatchingGroups :: Int -> Bool
+noLargerMatchingGroups n = length fixedMatchingGroups > 0
+  where
+    frequencies = digitFrequencies n
+    fixedMatchingGroups = Prelude.filter (\(_, freq) -> freq == 2) frequencies
+
 solvePartTwo :: IO Integer
-solvePartTwo = return 0
+solvePartTwo = return (fromIntegral (length result))
+  where
+    unchecked = Prelude.filter meetsBothCriteria [123257..647015]
+    result = Prelude.filter noLargerMatchingGroups unchecked
