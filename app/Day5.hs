7d744f0fce04625c20e89e9a4029fba7c88b2015
jontmy00 <jontmy001@gmail.com>
2021-05-05 17:27:45 +0800

    Complete day 5 part 2

--- app/Day5.hs
+++ app/Day5.hs
@@ -2,6 +2,7 @@ module Day5 where
 
 import Data.List.Split
 import InputReader
+import Debug.Trace
 
 import Data.Vector hiding ((++))
 
@@ -13,43 +14,90 @@ parseInput str = fromList lst
   where lst = Prelude.map read (splitOn "," str)
 
 getOpcode :: Position -> Vector Int -> Opcode
-getOpcode pos vec = opcode
+getOpcode pos vec = trace ("  opcode: " ++ show opcode) opcode
   where
     ins = Prelude.reverse (show $ (vec ! pos))
     opcode = read $ Prelude.reverse (Prelude.take 2 ins)
-    
-isPositional :: Position -> Int -> Bool
-isPositional pos ins
-  | pos >= (Prelude.length $ show ins) - 2 = True
-  | otherwise = (Prelude.reverse $ show ins) !! (pos + 2) == '0'
 
-operate :: Int -> Vector Int -> Vector Int
-operate pos vec =
-  case (getOpcode pos vec) of
-    1  -> operate (pos + 4) $ oppAdd pos vec
-    2  -> operate (pos + 4) $ opMultiply pos vec
-    3  -> operate (pos + 2) $ opStore pos 1 vec
-    4  -> operate (pos + 2) $ opRetrieve pos vec
+isPositional :: Position -> Int -> Bool
+isPositional pos ins = result
+  where
+    modes = Prelude.drop 2 $ Prelude.reverse (show ins)
+    result = if pos > Prelude.length modes then True else (modes !! (pos - 1)) == '0'
+
+-- In immediate mode, reads the value of the parameter.
+-- In position mode, reads the value from the pointer provided by the parameter.
+getValue :: Position -> Position -> Vector Int -> Int
+getValue insPos paramPos vec = value
+  where
+    ins = vec ! insPos
+    param = vec ! (insPos + paramPos)
+    value = if isPositional paramPos ins then vec ! param else param
+
+-- Parameters that an instruction writes to will never be in immediate mode.
+-- In position mode, overwrites the value stored at the index position with the value given.
+setValue :: Position -> Vector Int -> Int -> Vector Int
+setValue pos vec val = vec // [(vec ! pos, val)]
+
+operate :: Position -> Vector Int -> Int -> Vector Int
+operate pos vec input =
+  case (trace ("position: " ++ show pos ++ " (" ++ show (vec ! pos) ++ ")" ++ " @ " ++ show vec) $ getOpcode pos vec) of
+    1  -> operate (pos + 4) (oppAdd pos vec) input
+    2  -> operate (pos + 4) (opMultiply pos vec) input
+    3  -> operate (pos + 2) (opStore pos vec input) input
+    4  -> operate (pos + 2) (opRetrieve pos vec) input
+    5  -> operate (opJumpIfTrue pos vec) vec input
+    6  -> operate (opJumpIfFalse pos vec) vec input
+    7  -> operate (pos + 4) (opLessThan pos vec) input
+    8  -> operate (pos + 4) (opEquals pos vec) input
     99 -> vec
     _  -> error $ "parseOpcode :: Invalid opcode " ++ (show $ getOpcode pos vec) ++ " at position " ++ show pos
 
-opStore :: Position -> Int -> Vector Int -> Vector Int
-opStore pos input vec = vec // [(vec ! (pos + 1), input)]
+opJumpIfTrue :: Position -> Vector Int -> Position
+opJumpIfTrue pos vec = ptr
+  where
+    first = getValue pos 1 vec
+    second = getValue pos 2 vec
+    ptr = if first /= 0 then second else pos + 3
+
+opJumpIfFalse :: Position -> Vector Int -> Position
+opJumpIfFalse pos vec = ptr
+  where
+    first = getValue pos 1 vec
+    second = getValue pos 2 vec
+    ptr = if first == 0 then second else pos + 3
+
+opLessThan :: Position -> Vector Int -> Vector Int
+opLessThan pos vec = setValue (pos + 3) vec intBool
+  where
+    first = getValue pos 1 vec
+    second = getValue pos 2 vec
+    intBool = if first < second then 1 else 0
+
+opEquals :: Position -> Vector Int -> Vector Int
+opEquals pos vec = setValue (pos + 3) vec intBool
+ where
+   first = traceShow (getValue pos 1 vec) (getValue pos 1 vec)
+   second = traceShow (getValue pos 2 vec) (getValue pos 2 vec)
+   intBool = if first == second then 1 else 0
+
+opStore :: Position -> Vector Int -> Int -> Vector Int
+opStore pos vec input = vec // [(vec ! (pos + 1), input)]
 
 opRetrieve :: Position -> Vector Int -> Vector Int
 opRetrieve pos vec = output
-  where 
-    output = if isPositional 0 (vec ! pos)
+  where
+    output = if isPositional 1 (vec ! pos)
       then snoc vec (vec ! (vec ! (pos + 1)))
       else snoc vec (vec ! (pos + 1))
 
 oppAdd :: Position -> Vector Int -> Vector Int
 oppAdd pos vec = vec // [(vec ! (pos + 3), result)]
   where
-    first = if isPositional 0 (vec ! pos)
+    first = if isPositional 1 (vec ! pos)
       then vec ! (vec ! (pos + 1))
       else vec ! (pos + 1)
-    second = if isPositional 1 (vec ! pos)
+    second = if isPositional 2 (vec ! pos)
      then vec ! (vec ! (pos + 2))
      else vec ! (pos + 2)
     result = first + second
@@ -57,10 +105,10 @@ oppAdd pos vec = vec // [(vec ! (pos + 3), result)]
 opMultiply :: Position -> Vector Int -> Vector Int
 opMultiply pos vec = vec // [(vec ! (pos + 3), result)]
  where
-   first = if isPositional 0 (vec ! pos)
+   first = if isPositional 1 (vec ! pos)
      then vec ! (vec ! (pos + 1))
      else vec ! (pos + 1)
-   second = if isPositional 1 (vec ! pos)
+   second = if isPositional 2 (vec ! pos)
     then vec ! (vec ! (pos + 2))
     else vec ! (pos + 2)
    result = first * second
@@ -69,8 +117,12 @@ solvePartOne :: IO Integer
 solvePartOne =
   do
     input <- readStrings "app/input/Day5.txt"
-    let result = operate 0 $ parseInput (input !! 0)
+    let result = operate 0 (parseInput (input !! 0)) 1
     return (fromIntegral (Data.Vector.last result))
 
 solvePartTwo :: IO Integer
-solvePartTwo = return 0
+solvePartTwo =
+  do
+    input <- readStrings "app/input/Day5.txt"
+    let result = operate 0 (parseInput (input !! 0)) 5
+    return (fromIntegral (Data.Vector.last result))
