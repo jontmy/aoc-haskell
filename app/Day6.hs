bc094f5df3d77c1d1e0d923dff671fc591666562
jontmy00 <jontmy001@gmail.com>
2021-05-06 13:15:24 +0800

    Implement orbital string parsing

--- app/Day6.hs
+++ app/Day6.hs
@@ -1,7 +1,35 @@
 module Day6 where
+  
+import InputReader(readStrings)
+import Data.List.Split(splitOn)
+import Data.Map as Map
+import Data.Tree as Tree
+import Data.Graph as Graph
+
+type OrbitalPair = (OrbitalParent, OrbitalChild)
+type OrbitalParent = String
+type OrbitalChild = String
+
+parseOrbit :: String -> OrbitalPair
+parseOrbit str = (parent, child)
+  where
+    splits = splitOn ")" str
+    parent = head splits
+    child  = last splits
+    
+parseOrbits :: [String] -> Map OrbitalParent [OrbitalChild]
+parseOrbits strs = Prelude.foldr addOrbit Map.empty orbits
+  where orbits = Prelude.map parseOrbit strs
+
+addOrbit :: OrbitalPair -> Map OrbitalParent [OrbitalChild] -> Map OrbitalParent [OrbitalChild]
+addOrbit (parent, child) = insertWith (++) parent [child]
 
 solvePartOne :: IO Int
-solvePartOne = return 0
+solvePartOne =
+  do
+    input <- readStrings "app/input/Day5.txt"
+    let orbit = parseOrbits input
+    return 0
 
 solvePartTwo :: IO Int
 solvePartTwo = return 0
