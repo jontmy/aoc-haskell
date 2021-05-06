61a17b5f9ba21ed4470b8ccaeceb8f8ac67c3a4d
jontmy00 <jontmy001@gmail.com>
2021-05-06 16:50:09 +0800

    Implement ancestry determination

--- app/Day6.hs
+++ app/Day6.hs
@@ -2,14 +2,16 @@ module Day6 where
 
 import InputReader(readStrings)
 import Data.List.Split(splitOn)
+
 import Data.Map as Map
-import Data.Graph as Graph
+import Data.Set as Set
 
 type OrbitalParent = String
 type OrbitalChild = String
 
 type OrbitalPair = (OrbitalParent, OrbitalChild)
-type OrbitalMap = Graph
+type OrbitalMap = Map OrbitalParent [OrbitalChild]
+type InvertedOrbitalMap = Map OrbitalChild [OrbitalParent]
 
 -- Take an orbital string in the form of <parent>)<child> and returns an OrbitalPair tuple.
 parseOrbit :: String -> OrbitalPair
@@ -20,22 +22,15 @@ parseOrbit str = (parent, child)
     child  = last splits
 
 -- Takes a list of orbital strings and returns a map of each orbital parent to their orbital children.
-parseOrbits :: [String] -> Map OrbitalParent [OrbitalChild]
+parseOrbits :: [String] -> OrbitalMap
 parseOrbits strs = Prelude.foldr addOrbit Map.empty orbits
   where
     orbits = Prelude.map parseOrbit strs
     addOrbit (parent, child) = insertWith (++) parent [child]
-
--- Takes the tuples from parseOrbits and constructs an orbital map represented by a directed acyclic graph.
--- In the graph, parents and children are represented by vertices, while the directed edges are in the form parent -> children
-buildOrbitalMap :: Map OrbitalParent [OrbitalChild] -> (OrbitalMap, Vertex -> (OrbitalParent, OrbitalChild, [OrbitalChild]), OrbitalChild -> Maybe Vertex)
-buildOrbitalMap orbits = graphFromEdges $ format orbits
-  where
-    format = Prelude.map (\(parent, child) -> (parent, parent, child)) . toList
-
+    
 -- Calculates the number of direct and indirect orbital children by recursion, starting from the orbital parent given.
 calculateOrbits :: OrbitalParent -> Map OrbitalParent [OrbitalChild] -> Int
-calculateOrbits parent orbitalMap = if member parent orbitalMap then directOrbits + indirectOrbits else 0
+calculateOrbits parent orbitalMap = if Map.member parent orbitalMap then directOrbits + indirectOrbits else 0
   where
     children = orbitalMap ! parent
     directOrbits = length children
@@ -49,6 +44,44 @@ solvePartOne =
     let orbitalMap = parseOrbits input
     let orbitalBodies = keys orbitalMap
     return (sum $ Prelude.map (`calculateOrbits` orbitalMap) orbitalBodies)
+    
+-- Inverts a Data.Map, handling the case where a value appears more than once in the "right hand side" assignments.
+-- https://stackoverflow.com/questions/21538903/how-can-i-elegantly-invert-a-maps-keys-and-values#21541158
+invert :: (Ord k, Ord v) => Map k [v] -> Map v [k]
+invert m = Map.fromListWith (++) pairs
+    where pairs = [(v, [k]) | (k, vs) <- Map.toList m, v <- vs]
+
+-- Finds the parent(s) of an orbital body within an orbital map.
+parentsOf :: OrbitalChild -> InvertedOrbitalMap -> [OrbitalParent]
+parentsOf = findWithDefault []
+
+-- Finds all the ancestors of an orbital body within an orbital map.
+ancestorsOf :: [OrbitalChild] -> InvertedOrbitalMap -> Set OrbitalParent
+ancestorsOf children invMap = Set.union (Set.fromList parents) (ancestorsOf parents invMap)
+  where
+    parents = concatMap (`parentsOf` invMap) children
+
+-- Finds all the common ancestors of 2 orbital bodies within an orbital map.
+commonAncestorsOf :: OrbitalChild -> OrbitalChild -> InvertedOrbitalMap -> Set OrbitalParent
+commonAncestorsOf c1 c2 invMap = Set.intersection a1 a2
+  where
+    a1 = ancestorsOf [c1] invMap
+    a2 = ancestorsOf [c2] invMap
+
+-- Finds the minimum number of orbital transfers needed to get from one orbital body to another.
+-- Assumes that the first orbital body orbits the second orbital body, directly or indirectly.
+orbitalTransfers :: OrbitalChild -> OrbitalParent -> InvertedOrbitalMap -> Int
+orbitalTransfers child parent invMap = 0
+
+-- Finds the shortest path of orbital transfers between 2 orbital bodies by breadth-first search.
+-- Throws an error if there is no path of orbital transfers between the 2 bodies.
+
+
 
 solvePartTwo :: IO Int
-solvePartTwo = return 0
+solvePartTwo =
+  do
+    input <- readStrings "app/input/Day6.txt"
+    let invOrbitalMap = invert $ parseOrbits input -- child -> parent
+--    mapM_ print $ toList invOrbitalMap
+    return 0
